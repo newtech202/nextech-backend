58737050c41369fa7ea53885298c5d51acba9a29
Antinio Machado <antonio.machado@newtech.ao>
2025-02-06 15:23:46 +0100

    config/auth resolved

--- src/middlewares/auth.ts
+++ src/middlewares/auth.ts
@@ -1,6 +1,7 @@
 import { NextFunction, Request, Response } from "express";
 import jwt from "jsonwebtoken";
 import auth from "../config/auth";
+ 
  declare module "express-serve-static-core" {
   interface Request {
     session?: Session;
